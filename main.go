b7c6e4b37de257444a93f548b922a52d6355a9d7
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-04-18 14:10:45 -0300

    browser interface

--- main.go
+++ main.go
@@ -12,19 +12,18 @@ func main() {
 		log.Fatalf("error in main: %v", err)
 	}
 
-	defer playwrightWrapper.Playwright.Stop()
+	defer playwrightWrapper.Stop()
 
 	browser, err := playwrightWrapper.NewBrowser(false)
 	if err != nil {
 		log.Fatalf("error in main: %v", err)
 	}
-
 	defer browser.Close()
 
 	// Create a new page
 	page, err := browser.NewPage()
 	if err != nil {
-		log.Fatalf("Could not create page: %v", err)
+		log.Fatalf("error in main: %v", err)
 	}
 	defer page.Close()
 
@@ -34,6 +33,10 @@ func main() {
 	}
 
 	fmt.Println("Successfully opened www.facebook.com in Chromium")
+
+	// Select email input field
+	emailInput := page.Locator("input[name=email]")
+	fmt.Println("Selected email input:", emailInput)
 
 	time.Sleep(10 * time.Minute)
 }
--- playwright.go
+++ playwright.go
@@ -6,22 +6,55 @@ import (
 	"github.com/playwright-community/playwright-go"
 )
 
+type BrowserWrapperInterface interface {
+	NewPage() (playwright.Page, error)
+	Close() error
+}
+
 type PlaywrightWrapperInterface interface {
 	Run() error
-	NewBrowser(headless bool) playwright.Browser
+	NewBrowser(headless bool) (BrowserWrapperInterface, error)
+	Stop() error
+}
+
+type BrowserWrapper struct {
+	Browser playwright.Browser
 }
 
 type PlaywrightWrapper struct {
 	Playwright *playwright.Playwright
 }
 
-func NewPlaywrightWrapper() (*PlaywrightWrapper, error) {
+func NewBrowserWrapper(browser playwright.Browser) (BrowserWrapperInterface, error) {
+	browserWrapper := BrowserWrapper{
+		Browser: browser,
+	}
+	return &browserWrapper, nil
+}
+
+func (ww *BrowserWrapper) NewPage() (playwright.Page, error) {
+	page, err := ww.Browser.NewPage()
+	if err != nil {
+		return page, fmt.Errorf("Could not create page: %v", err)
+	}
+	return page, nil
+}
+
+func (ww *BrowserWrapper) Close() error {
+	return ww.Browser.Close()
+}
+
+func NewPlaywrightWrapper() (PlaywrightWrapperInterface, error) {
 	playwrightWrapper := PlaywrightWrapper{}
 	err := playwrightWrapper.Run()
 	if err != nil {
 		return &playwrightWrapper, fmt.Errorf("Could not create PlaywrightWrapper: %v", err)
 	}
 	return &playwrightWrapper, nil
+}
+
+func (pw *PlaywrightWrapper) Stop() error {
+	return pw.Playwright.Stop()
 }
 
 func (pw *PlaywrightWrapper) Run() error {
@@ -47,14 +80,16 @@ func (pw *PlaywrightWrapper) Run() error {
 	return err
 }
 
-func (pw *PlaywrightWrapper) NewBrowser(headless bool) (playwright.Browser, error) {
-	// Launch chromium browser
+func (pw *PlaywrightWrapper) NewBrowser(headless bool) (BrowserWrapperInterface, error) {
 	browser, err := pw.Playwright.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
 		Headless: &headless,
 	})
 	if err != nil {
-		return browser, fmt.Errorf("Could not launch chromium: %v", err)
+		return nil, fmt.Errorf("Could not launch chromium: %v", err)
 	}
-
-	return browser, nil
+	browserWrapper, nil := NewBrowserWrapper(browser)
+	if err != nil {
+		return browserWrapper, fmt.Errorf("Could not create BrowserWrapper: %v", err)
+	}
+	return browserWrapper, nil
 }
