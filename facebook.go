ee936f11e19b28a24799b70c8e8d6794da52681d
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-04-18 15:16:15 -0300

    login

--- facebook.go
+++ facebook.go
@@ -1,12 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 type FacebookScrapperInterface interface {
-	Login(username string, password string)
+	Login(userCredentials UserCredentials) error
 }
 
 type FacebookScrapper struct {
+	Browser BrowserWrapperInterface
 }
 
-func (fs *FacebookScrapper) Login(username string, passwrod string) {
+func NewFacebookScrapper(browser BrowserWrapperInterface) FacebookScrapperInterface {
+	facebookScrapper := FacebookScrapper{
+		Browser: browser,
+	}
+	return &facebookScrapper
+}
 
+func (fs *FacebookScrapper) Login(userCredentials UserCredentials) error {
+	page, err := fs.Browser.NewPage()
+	if err != nil {
+		return fmt.Errorf("error NewPage: %v", err)
+	}
+	defer page.Close()
+
+	url := "https://www.facebook.com"
+
+	// Navigate to Facebook
+	if err = page.Goto(url); err != nil {
+		return fmt.Errorf("error Goto: %v", err)
+	}
+
+	fmt.Printf("Successfully opened %s in Chromium\n", url)
+
+	emailInput := page.Locator("input[name=email]")
+	emailInput.Fill(userCredentials.Username)
+
+	passwordInput := page.Locator("input[name=pass]")
+	passwordInput.Fill(userCredentials.Password)
+
+	time.Sleep(10 * time.Minute)
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,49 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"time"
 )
 
 func main() {
 	config, err := NewConfig()
 	if err != nil {
-		log.Fatalf("error in main: %v", err)
+		log.Fatalf("error NewConfig: %v", err)
 	}
 
 	playwrightWrapper, err := NewPlaywrightWrapper()
 	if err != nil {
-		log.Fatalf("error in main: %v", err)
+		log.Fatalf("error NewPlaywrightWrapper: %v", err)
 	}
 
 	defer playwrightWrapper.Stop()
 
 	browser, err := playwrightWrapper.NewBrowser(false)
 	if err != nil {
-		log.Fatalf("error in main: %v", err)
+		log.Fatalf("error NewBrowser: %v", err)
 	}
 	defer browser.Close()
 
-	// Create a new page
-	page, err := browser.NewPage()
-	if err != nil {
-		log.Fatalf("error in main: %v", err)
-	}
-	defer page.Close()
+	facebookScrapper := NewFacebookScrapper(browser)
 
-	// Navigate to Facebook
-	if err = page.Goto("https://www.facebook.com"); err != nil {
-		log.Fatalf("error in main: %v", err)
-	}
-
-	fmt.Println("Successfully opened www.facebook.com in Chromium")
-
-	// Select email input field
-	emailInput := page.Locator("input[name=email]")
-	fmt.Println("Selected email input:", emailInput)
-
-	emailInput.Fill(config.UserCredentials.Username)
-
-	time.Sleep(10 * time.Minute)
+	facebookScrapper.Login(config.UserCredentials)
 }
