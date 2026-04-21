1a5862142adf59f6585e264cdd9a3bde4ee878f8
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-04-21 15:03:08 -0300

    6 digits

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func WriteRandomJsonFile(body []byte) error {
 	timestamp := time.Now().UnixNano()
 	random := rand.Intn(1000000)
 
-	filename := filepath.Join("data", fmt.Sprintf("response_%d_%d.json", timestamp, random))
+	filename := filepath.Join("data", fmt.Sprintf("response_%d_%06d.json", timestamp, random))
 
 	if err := os.WriteFile(filename, body, 0644); err != nil {
 		return err
@@ -28,23 +28,24 @@ func WriteRandomJsonFile(body []byte) error {
 
 func WriteJsonResponse(body []byte) error {
 	var jsonData interface{}
-	if err := json.Unmarshal(body, &jsonData); err != nil {
-		// Try parsing as newline-delimited JSON
-		lines := strings.Split(string(body), "\n")
-		for _, line := range lines {
-			if strings.TrimSpace(line) == "" {
-				continue
-			}
-			lineByte := []byte(line)
-			var lineData interface{}
-			if err := json.Unmarshal(lineByte, &lineData); err == nil {
-				WriteRandomJsonFile(lineByte)
-			}
-		}
-		return nil
+	if err := json.Unmarshal(body, &jsonData); err == nil {
+		return WriteRandomJsonFile(body)
 	}
 
-	return WriteRandomJsonFile(body)
+	var lineData interface{}
+	lines := strings.Split(string(body), "\n")
+	for _, line := range lines {
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		lineByte := []byte(line)
+		if err := json.Unmarshal(lineByte, &lineData); err == nil {
+			WriteRandomJsonFile(lineByte)
+		}
+	}
+
+	return nil
+
 }
 
 func main() {
