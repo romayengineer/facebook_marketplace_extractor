fa0ef62fcd3a39dcd38e6b46ccb150656a0bb571
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-04-25 09:54:44 -0300

    file store

--- src/product.go
+++ src/product.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"reflect"
 )
 
 type AttributeData struct {
@@ -59,71 +58,6 @@ func NewMarketplaceItemDetails(
 		SellerName:    sellerName,
 		Photos:        photos,
 	}
-}
-
-type FileStoreImpl[T any] struct {
-	id       string
-	filePath string
-	data     *T
-}
-
-type FileStore[T any] interface {
-	Get() error
-	Save(data T) error
-}
-
-func NewProductFileStore(productId string) FileStore[MarketplaceItemDetails] {
-	fileName := fmt.Sprintf("detail_%v.json", productId)
-	filePath := filepath.Join("data", fileName)
-	return &FileStoreImpl[MarketplaceItemDetails]{id: productId, filePath: filePath}
-}
-
-func (pfs *FileStoreImpl[T]) Get() error {
-	content, err := os.ReadFile(pfs.filePath)
-	if err != nil {
-		return err
-	}
-
-	var data T
-	if err := json.Unmarshal(content, &data); err == nil {
-		return err
-	}
-
-	pfs.data = &data
-
-	return nil
-}
-
-func (pfs *FileStoreImpl[T]) Save(data T) error {
-	if pfs.data == nil {
-		err := pfs.Get()
-		if err != nil {
-			return err
-		}
-	}
-
-	pfsValue := reflect.ValueOf(pfs.data).Elem()
-	dataValue := reflect.ValueOf(data)
-
-	for i := 0; i < pfsValue.NumField(); i++ {
-		pfsField := pfsValue.Field(i)
-		dataField := dataValue.Field(i)
-
-		if !dataField.IsNil() {
-			pfsField.Set(dataField)
-		}
-	}
-
-	indented, err := json.MarshalIndent(pfs.data, "", "  ")
-	if err != nil {
-		return err
-	}
-
-	if err := os.WriteFile(pfs.filePath, indented, 0644); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func GetProductDetails(data any) (*MarketplaceItemDetails, error) {
