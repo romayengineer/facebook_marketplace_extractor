2dbe1273c1df4d26829d0da5f75905a9dfbaba09
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2026-05-14 12:38:54 -0300

    file

--- src/files.go
+++ src/files.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
+	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 )
@@ -43,4 +46,70 @@ func WriteRandomJsonFile(prefix string, friendly_name string, body []byte) error
 func WriteRandomJsonFileIndented(prefix string, friendly_name string, jsonData any) error {
 	indented, _ := json.MarshalIndent(jsonData, "", "  ")
 	return WriteRandomJsonFile(prefix, friendly_name, indented)
+}
+
+func ForEachJsonInData(prefix string, process func(filePath string, jsonData any), sortit bool) {
+	// open and read all files in data folder that start with response and end in .json
+
+	filePaths := []string{}
+
+	err := filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		fileName := d.Name()
+
+		if !strings.HasPrefix(fileName, prefix) || !strings.HasSuffix(fileName, ".json") {
+			return nil
+		}
+
+		filePaths = append(filePaths, path)
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("error reading data directory: %v", err)
+	}
+
+	if sortit {
+		// if sort is true sort filePaths in ascendant order
+		sort.Strings(filePaths)
+	} else {
+		// else sort filePaths in random order
+		rand.Shuffle(len(filePaths), func(i, j int) { filePaths[i], filePaths[j] = filePaths[j], filePaths[i] })
+	}
+
+	for _, filePath := range filePaths {
+
+		// fmt.Printf("%s\n", filePath)
+
+		body, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Printf("error reading file %s: %v", filePath, err)
+			continue
+		}
+
+		var jsonData any
+		if err := json.Unmarshal(body, &jsonData); err != nil {
+			log.Printf("error parsing JSON from %s: %v", filePath, err)
+			continue
+		}
+
+		process(filePath, jsonData)
+	}
+
+}
+
+func ForEachResponse(process func(filePath string, jsonData any), sortit bool) {
+	ForEachJsonInData("response_", process, sortit)
+}
+
+func ForEachDetail(process func(filePath string, jsonData any), sortit bool) {
+	ForEachJsonInData("detail_", process, sortit)
 }
--- src/main.go
+++ src/main.go
@@ -4,13 +4,9 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/fs"
 	"log"
-	"math/rand"
 	"net/url"
 	"os"
-	"path/filepath"
-	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -362,72 +358,6 @@ func GetDetails() {
 	}, false)
 
 	WaitingForInput()
-}
-
-func ForEachJsonInData(prefix string, process func(filePath string, jsonData any), sortit bool) {
-	// open and read all files in data folder that start with response and end in .json
-
-	filePaths := []string{}
-
-	err := filepath.WalkDir("data", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		fileName := d.Name()
-
-		if !strings.HasPrefix(fileName, prefix) || !strings.HasSuffix(fileName, ".json") {
-			return nil
-		}
-
-		filePaths = append(filePaths, path)
-
-		return nil
-	})
-
-	if err != nil {
-		log.Fatalf("error reading data directory: %v", err)
-	}
-
-	if sortit {
-		// if sort is true sort filePaths in ascendant order
-		sort.Strings(filePaths)
-	} else {
-		// else sort filePaths in random order
-		rand.Shuffle(len(filePaths), func(i, j int) { filePaths[i], filePaths[j] = filePaths[j], filePaths[i] })
-	}
-
-	for _, filePath := range filePaths {
-
-		// fmt.Printf("%s\n", filePath)
-
-		body, err := os.ReadFile(filePath)
-		if err != nil {
-			log.Printf("error reading file %s: %v", filePath, err)
-			continue
-		}
-
-		var jsonData any
-		if err := json.Unmarshal(body, &jsonData); err != nil {
-			log.Printf("error parsing JSON from %s: %v", filePath, err)
-			continue
-		}
-
-		process(filePath, jsonData)
-	}
-
-}
-
-func ForEachResponse(process func(filePath string, jsonData any), sortit bool) {
-	ForEachJsonInData("response_", process, sortit)
-}
-
-func ForEachDetail(process func(filePath string, jsonData any), sortit bool) {
-	ForEachJsonInData("detail_", process, sortit)
 }
 
 func SaveProductsIfAny(products []MarketplaceItemDetails) bool {
